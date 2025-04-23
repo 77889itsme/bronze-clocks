9550a411f1addfcbac21c522937f43619c500916
your name goes here <133010759+77889itsme@users.noreply.github.com>
2025-04-23 11:23:05 -0400

    feat: update highlight current page function in timeline

--- static/js/learn_motif.js
+++ static/js/learn_motif.js
@@ -1,19 +1,3 @@
-function attachLinks() {
-    let linkList = [
-        { selector: "#shang-group", url: "/learn/motif/1" },
-        { selector: "#zhou-west-group", url: "/learn/motif/2" },
-        { selector: "#zhou-east-group", url: "/learn/motif/3" },
-        { selector: "#qinhan-group", url: "/learn/motif/4" }
-    ];
-
-    linkList.forEach(item => {
-        $(item.selector).css("cursor", "pointer");
-        $(item.selector).click(() => {
-            window.location.href = item.url;
-        });
-    });
-}
-
 $(document).ready(function () {
     attachLinks()
 });  
--- static/js/learn_shape.js
+++ static/js/learn_shape.js
@@ -1,19 +1,3 @@
-function attachLinks() {
-    let linkList = [
-        { selector: "#shang-group", url: "/learn/shape/1" },
-        { selector: "#zhou-west-group", url: "/learn/shape/2" },
-        { selector: "#zhou-east-group", url: "/learn/shape/3" },
-        { selector: "#qinhan-group", url: "/learn/shape/4" }
-    ];
-
-    linkList.forEach(item => {
-        $(item.selector).css("cursor", "pointer");
-        $(item.selector).click(() => {
-            window.location.href = item.url;
-        });
-    });
-}
-
 $(document).ready(function () {
     attachLinks()
 });  
