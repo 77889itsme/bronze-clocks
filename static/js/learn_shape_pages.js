83b45c923a74224290f4c491ea3232246358d961
your name goes here <133010759+77889itsme@users.noreply.github.com>
2025-04-23 10:38:30 -0400

    feat: add buttons in learning module to control pages

--- static/js/learn_shape_pages.js
+++ static/js/learn_shape_pages.js
@@ -14,8 +14,40 @@ function attachLinks() {
     });
 }
 
+function updateButtons(){
+    let prevBtn = "";
+    if (currentPage > 1) {
+        prevBtn = `<a href="/learn/shape/${currentPage - 1}" class="btn btn-light">Previous</a>`;
+    }
+    
+    let nextBtn = "";
+    if (currentPage < 4) {
+        nextBtn = `<a href="/learn/shape/${currentPage + 1}" class="btn btn-light">Next</a>`;
+    }
+    
+    $('#navButtonPrevious').html(`
+        <div>${prevBtn}</div>
+    `);
+
+    $('#navButtonNext').html(`
+        <div>${nextBtn}</div>
+    `);
+
+    if (currentPage === 4) {
+        quizBtn = `<a href="/quiz" class="btn btn-primary">Take the Quiz</a>`;
+        learnPageBtn = `<a href="/learn" class="btn btn-light">Choose Another Section</a>`;
+
+        $('#final-page').html(`
+            <div>${quizBtn}</div>
+            <div>${learnPageBtn}</div>
+        `)
+    }
+}
+
 
 function updateView(){
+    updateButtons();
+
     let title = info.title;
     $('#shape-title').html(title);
 
@@ -27,7 +59,6 @@ function updateView(){
 
     let imgPath = info.image_path; 
     $('#shape-image').attr('src', '/' + imgPath);
-    
 }
 
 $(document).ready(function () {
